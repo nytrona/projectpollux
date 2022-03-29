24d4121974932b873404146a3c0f69309a6578b0
Dyvergence <100308708+Dyvergence@users.noreply.github.com>
2022-03-29 17:26:55 -0500

    Re-add item_healthcharger and item_suitcharger
    Signed-off-by: Nytrona <100308708+Nytrona@users.noreply.github.com>

--- pollux/Entities/Charger.cs
+++ pollux/Entities/Charger.cs
@@ -74,7 +74,7 @@ namespace ProjectPollux.Entities
 		/// Name of the sound to play if this charger's juice gets refilled for any reason.
 		/// </summary>
 		[Property( "chargerrefilledSound", Title = "Juice Refilled Sound" ), FGDType( "sound" ), Category( "Sound Settings" )]
-		public string ChargerRefilledFound { get; set; } = "HealthChargerSounds.Recharge";
+		public string ChargerRefilledSound { get; set; } = "HealthChargerSounds.Recharge";
 
 		// what to replenish
 
@@ -141,7 +141,7 @@ namespace ProjectPollux.Entities
 			if ( IsClient )
 				return;
 
-			PlaySound( ChargerRefilledFound );
+			PlaySound( ChargerRefilledSound );
 
 			UpdateJuice( MaxJuice );
 		}
@@ -255,7 +255,7 @@ namespace ProjectPollux.Entities
 				}
 				return false;
 			}
-			else if ( (ReplenishesArmor && !ReplenishesHealth) && (Player.ArmorValue >= MaxPlayerArmor)) // this charger replenishes only armor & player's armor is above or equal to max, so deny charging
+			else if ( (ReplenishesArmor && !ReplenishesHealth) && (Player.ArmorValue >= MaxPlayerArmor) ) // this charger replenishes only armor & player's armor is above or equal to max, so deny charging
 			{
 				if ( SoundTime <= Time.Now )
 				{
@@ -301,7 +301,7 @@ namespace ProjectPollux.Entities
 					Player.IncrementHealthValue( AmountToIncrementHealthBy, MaxPlayerHealth );
 					UpdateAnim();
 				}
-				else if (Player.ArmorValue < MaxPlayerArmor && Player.Health >= MaxPlayerHealth ) // player's armor value is below max, but health is either at or above max
+				else if ( Player.ArmorValue < MaxPlayerArmor && Player.Health >= MaxPlayerHealth ) // player's armor value is below max, but health is either at or above max
 				{
 					UpdateJuice( Juice - AmountToIncrementArmorBy );
 					Player.IncrementArmorValue( AmountToIncrementArmorBy, MaxPlayerArmor );
@@ -314,7 +314,7 @@ namespace ProjectPollux.Entities
 					UpdateAnim();
 				}
 			}
-			else if ( ReplenishesArmor && !ReplenishesHealth  ) // this charger replenishes only armor
+			else if ( ReplenishesArmor && !ReplenishesHealth ) // this charger replenishes only armor
 			{
 				if ( Player.ArmorValue < MaxPlayerArmor )
 				{
@@ -359,6 +359,130 @@ namespace ProjectPollux.Entities
 		}
 
 		public bool IsUsable( Entity user ) { return true; }
+	}
+
+	[Library( "item_healthcharger", Description = "A preconfigured ent_charger with appropriate settings for a health charger." )]
+	[Hammer.Model( Model = "models/props_combine/health_charger001.vmdl" )]
+	[Hammer.EntityTool( "Health Station", "Items", "An entity that gradually replenishes health." )]
+	[Hammer.VisGroup( Hammer.VisGroup.Dynamic )]
+	[Hammer.SkipProperty( "chargerlocation" )]
+	[Hammer.SkipProperty( "maxplayerhealth" )]
+	[Hammer.SkipProperty( "maxplayerjuice" )]
+	[Hammer.SkipProperty( "chargertype" )]
+	[Hammer.SkipProperty( "usesskill" )]
+	[Hammer.SkipProperty( "juice" )]
+	[Hammer.SkipProperty( "maxjuice" )]
+	[Hammer.SkipProperty( "replenishhealth" )]
+	[Hammer.SkipProperty( "replenisharmor" )]
+	[Hammer.SkipProperty( "startchargesound" )]
+	[Hammer.SkipProperty( "chargeloopsound" )]
+	[Hammer.SkipProperty( "chargedenysound" )]
+	[Hammer.SkipProperty( "chargerrefilledsound" )]
+	[Hammer.SkipProperty( "amounttoincrementarmorby" )]
+	[Hammer.SkipProperty( "amounttoincrementhealthby" )]
+	partial class ItemHealthCharger : ItemPlayerCharger
+	{
+		public override void Spawn()
+		{
+			base.Spawn();
+			ReplenishesHealth = true;
+			ReplenishesArmor = false;
+			StartChargeSound = "HealthChargerSounds.Start";
+			ChargeLoopSound = "HealthChargerSounds.Loop";
+			ChargeDenySound = "HealthChargerSounds.Deny";
+			ChargerRefilledSound = "HealthChargerSounds.Refill";
+			AmountToIncrementHealthBy = 1;
+			Juice = 50;
+			MaxJuice = 50;
+
+			SetModel( "models/props_combine/health_charger001.vmdl" );
+		}
+	}
+
+	[Library( "item_suitcharger", Description = "A preconfigured ent_charger with appropriate settings for a armor charger." )]
+	[Hammer.Model( Model = "models/props_combine/suit_charger001.vmdl" )]
+	[Hammer.EntityTool( "Armor Charging Station", "Items", "An entity that gradually replenishes armor." )]
+	[Hammer.VisGroup( Hammer.VisGroup.Dynamic )]
+	[Hammer.SkipProperty( "chargerlocation" )]
+	[Hammer.SkipProperty( "maxplayerhealth" )]
+	[Hammer.SkipProperty( "maxplayerjuice" )]
+	[Hammer.SkipProperty( "chargertype" )]
+	[Hammer.SkipProperty( "usesskill" )]
+	[Hammer.SkipProperty( "juice" )]
+	[Hammer.SkipProperty( "maxjuice" )]
+	[Hammer.SkipProperty( "replenishhealth" )]
+	[Hammer.SkipProperty( "replenisharmor" )]
+	[Hammer.SkipProperty( "startchargesound" )]
+	[Hammer.SkipProperty( "chargeloopsound" )]
+	[Hammer.SkipProperty( "chargedenysound" )]
+	[Hammer.SkipProperty( "chargerrefilledsound" )]
+	[Hammer.SkipProperty( "amounttoincrementarmorby" )]
+	[Hammer.SkipProperty( "amounttoincrementhealthby" )]
+	partial class ItemSuitCharger : ItemPlayerCharger
+	{
+		[Spawnflags]
+		public enum Spawnflags
+		{
+			CitadelRecharger = 8192,
+			KleinerRecharger = 16384
+		}
+
+		public new int Juice()
+		{
+			if ( SpawnFlags.Has( Spawnflags.CitadelRecharger ) )
+				return 500;
+			else if ( SpawnFlags.Has( Spawnflags.KleinerRecharger ) )
+				return 25;
+			else
+				return 75;
+		}
+
+		public new int MaxJuice()
+		{
+			if ( SpawnFlags.Has( Spawnflags.CitadelRecharger ) )
+				return 500;
+			else
+				return 100;
+		}
+
+		public new int AmountToIncrementArmorBy()
+		{
+			if ( SpawnFlags.Has( Spawnflags.CitadelRecharger ) )
+				return 10;
+			else
+				return 1;
+		}
+
+		public new bool ReplenishesHealth()
+		{
+			if ( SpawnFlags.Has( Spawnflags.CitadelRecharger ) )
+				return true;
+			else
+				return false;
+		}
+
+		public new int MaxPlayerArmor()
+		{
+			if ( SpawnFlags.Has( Spawnflags.CitadelRecharger ) )
+				return 200;
+			else
+				return 100;
+		}
+		public override void Spawn()
+		{
+			base.Spawn();
+			base.Juice = Juice();
+			base.MaxJuice = MaxJuice();
+			base.ReplenishesHealth = ReplenishesHealth();
+			base.AmountToIncrementArmorBy = AmountToIncrementArmorBy();
+			base.MaxPlayerArmor = MaxPlayerArmor();
+			ReplenishesArmor = true;
+			AmountToIncrementHealthBy = 5; // only increments if citadel recharger
+			StartChargeSound = "ArmorChargerSounds.Start";
+			ChargeLoopSound = "ArmorChargerSounds.Loop";
+			ChargeDenySound = "ArmorChargerSounds.Deny";
+			ChargerRefilledSound = "ArmorChargerSounds.Refill";
+		}
 	}
 
 	[Library( "dbg_ent_deathcharger", Description = "An entity that gradually deals damage to its user. For debugging purposes." )]
